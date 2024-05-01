1d5d9118ee562a0d974294cf5c6eddc3ae6e4378
GabrielOchieng <ogingagabriel@gmail.com>
2024-05-01 09:23:19 +0300

    responsive navbar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -16,9 +18,17 @@ const Header = () => {
             alt="logo"
           />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavItems = () => {
+  return <ul className="md:flex-between flex "></ul>;
+};
+
+export default NavItems;
