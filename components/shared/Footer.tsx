7bd6df57db35816bc170f1f0f2b77a596b751193
GabrielOchieng <ogingagabriel@gmail.com>
2024-05-01 11:39:35 +0300

    landing page setup

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>&copy; 2024 THEGABRIELSHOW. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
