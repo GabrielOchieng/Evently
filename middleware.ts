891f440f3b7b168ea2df3be5778a2dd811ea4eca
GabrielOchieng <ogingagabriel@gmail.com>
2024-05-01 08:57:39 +0300

    signup setup with clerk

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+  // ignoredRoutes: [
+  //     '/api/webhook/clerk',
+  //     '/api/webhook/stripe',
+  //     '/api/uploadthing',
+  // ]
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
